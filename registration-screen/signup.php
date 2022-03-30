ac2f2b3d37a7e983ec6ee7fa71ce7b793645502e
John Otilla <92242338+JohnOtilla09@users.noreply.github.com>
2022-03-30 16:23:12 +0800

    Update signup.php

--- registration-screen/signup.php
+++ registration-screen/signup.php
@@ -29,6 +29,9 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
+	<!-- Sweer Alert -->
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>	
+	
 </head>
 <body>
 
@@ -225,6 +228,14 @@
 					echo "</div>";
 				}
 			?>
+			
+			<?php if (isset($_POST['submit']) && isset($firstname) && isset($lastname) && isset($firstname) && isset($username) && isset($firstname) && isset($password) && isset($email) && isset($firstname) && isset($agree_terms)): ?>
+
+				<script>
+					swal("Successfully Registered", "", "success");
+				</script>
+
+			<?php endif; ?>	
 
 		</div>
 
