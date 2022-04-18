58627ea4c0d32c9ad3d8a94791dc67266fcc4916
JeffBeckLim <101444919+JeffBeckLim@users.noreply.github.com>
2022-04-18 11:28:32 +0800

    Update login_screen.php

--- login-screen/login_screen.php
+++ login-screen/login_screen.php
@@ -1,6 +1,6 @@
 <?php
 
-   	$isPost = $_SERVER["REQUEST_METHOD"] === "POST";
+    $isPost = $_SERVER["REQUEST_METHOD"] === "POST";
     $username_log = $_POST['username_log'] ?? null;
     $password_log = $_POST['password_log'] ?? null;
   
@@ -35,14 +35,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.88.1">
     <title>Log in Screen</title>
 
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     
     <!--FONT FAMILY -->
